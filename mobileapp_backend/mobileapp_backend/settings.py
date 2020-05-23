cc59ca90c5e32e5b1fb9cd4b90b9c6490a0d5ade
YarigaOleg <Oyariga@gmail.com>
2020-05-23 20:25:08 +0300

    Add postgres conf to settings.py

--- mobileapp_backend/mobileapp_backend/settings.py
+++ mobileapp_backend/mobileapp_backend/settings.py
@@ -80,7 +80,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'your_db_name',
+#         'USER': 'postgres',
+#         'PASSWORD': 'your_db_user_password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
